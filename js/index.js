ebea46f8b4f645323334406bb514006ad095ff95
messter10 <rumessa99@gmail.com>
2021-10-09 23:28:47 +0300

    half done

---
+++ js/index.js
@@ -0,0 +1,139 @@
+var menu_open = False;
+
+function ToggleBurger(x) {
+    const line1 = document.querySelector('.line1');
+    const line2 = document.querySelector('.line2');
+    const line3 = document.querySelector('.line3');
+    const nav_right = document.querySelector('.nav-right');
+    const burger = document.querySelector('.burger');
+    
+    if(!menu_open)
+    {
+        line1.classList.add('change');
+        line2.classList.add('change');
+        line3.classList.add('change');
+        nav_right.classList.add('open');
+        menu_open=true;
+    }
+    else
+    {
+        line1.classList.remove('change');
+        line2.classList.remove('change');
+        line3.classList.remove('change');
+        nav_right.classList.remove('open');
+        menu_open=false;
+    }
+}
+
+// sliders function
+function slide(wrapper, items, prev, next) {
+    var posX1 = 0,
+        posX2 = 0,
+        posInitial,
+        posFinal,
+        threshold = 100,
+        slides = items.getElementsByClassName('slide'),
+        slidesLength = slides.length,
+        slideSize = items.getElementsByClassName('slide')[0].offsetWidth,
+        firstSlide = slides[0],
+        lastSlide = slides[slidesLength - 1],
+        cloneFirst = firstSlide.cloneNode(true),
+        cloneLast = lastSlide.cloneNode(true),
+        index = 0,
+        allowShift = true;
+    
+    // Clone first and last slide
+    items.appendChild(cloneFirst);
+    items.insertBefore(cloneLast, firstSlide);
+    wrapper.classList.add('loaded');
+    
+    // Mouse and Touch events
+    items.onmousedown = dragStart;
+    
+    // Touch events
+    items.addEventListener('touchstart', dragStart);
+    items.addEventListener('touchend', dragEnd);
+    items.addEventListener('touchmove', dragAction);
+    
+    // Click events
+    prev.addEventListener('click', function () { shiftSlide(-1) });
+    next.addEventListener('click', function () { shiftSlide(1) });
+    
+    // Transition events
+    items.addEventListener('transitionend', checkIndex);
+    
+    function dragStart (e) {
+      e = e || window.event;
+      e.preventDefault();
+      posInitial = items.offsetLeft;
+      
+      if (e.type == 'touchstart') {
+        posX1 = e.touches[0].clientX;
+      } else {
+        posX1 = e.clientX;
+        document.onmouseup = dragEnd;
+        document.onmousemove = dragAction;
+      }
+    }
+  
+    function dragAction (e) {
+      e = e || window.event;
+      
+      if (e.type == 'touchmove') {
+        posX2 = posX1 - e.touches[0].clientX;
+        posX1 = e.touches[0].clientX;
+      } else {
+        posX2 = posX1 - e.clientX;
+        posX1 = e.clientX;
+      }
+      items.style.left = (items.offsetLeft - posX2) + "px";
+    }
+    
+    function dragEnd (e) {
+      posFinal = items.offsetLeft;
+      if (posFinal - posInitial < -threshold) {
+        shiftSlide(1, 'drag');
+      } else if (posFinal - posInitial > threshold) {
+        shiftSlide(-1, 'drag');
+      } else {
+        items.style.left = (posInitial) + "px";
+      }
+  
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+    
+    function shiftSlide(dir, action) {
+      items.classList.add('shifting');
+      
+      if (allowShift) {
+        if (!action) { posInitial = items.offsetLeft; }
+  
+        if (dir == 1) {
+          items.style.left = (posInitial - slideSize) + "px";
+          index++;      
+        } else if (dir == -1) {
+          items.style.left = (posInitial + slideSize) + "px";
+          index--;      
+        }
+      };
+      
+      allowShift = false;
+    }
+      
+    function checkIndex (){
+      items.classList.remove('shifting');
+  
+      if (index == -1) {
+        items.style.left = -(slidesLength * slideSize) + "px";
+        index = slidesLength - 1;
+      }
+  
+      if (index == slidesLength) {
+        items.style.left = -(1 * slideSize) + "px";
+        index = 0;
+      }
+      
+      allowShift = true;
+    }
+}
